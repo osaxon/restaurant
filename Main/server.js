9ec6e1c92fa3b94d9e7395724d898747439daba5
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-01 11:36:33 +0100

    FEAT: Server set up using express. Basic routes configured for index and reservation html pages

--- Main/server.js
+++ Main/server.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
+
+const reservations = [];
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,"index.html")))
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname,"reserve.html")))
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
