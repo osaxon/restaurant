4a28298245fb0284350c7427c1a9be9c994b0795
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-01 13:10:34 +0100

    FEAT: Added jQuery script to index.html. Created event listener to route to table API page where the reservation object array is rendered as json

--- Main/server.js
+++ Main/server.js
@@ -2,11 +2,28 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3100;
 
-const reservations = [];
+const reservations = [
+    {
+        customerName: "Oli Saxon",
+        TabaleID: 71,
+        time: "18:00",
+        partySize: 6
+    },
+    {
+        customerName: "Oli Saxon",
+        TabaleID: 71,
+        time: "20:00",
+        partySize: 6
+    },
+];
+const waitList = [];
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,"index.html")))
 app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname,"reserve.html")))
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname,"tables.html")))
+
+app.get('/api/tables', (req, res) => res.json(reservations))
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
